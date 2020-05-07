237b53f77bf17ec5dfc8d551e002d67c9a902932
Sh031224 <1cktmdgh2@gmail.com>
2020-05-07 14:05:07 +0900

    [ADD] React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SchoolMealsPage from "./pages/SchoolMealsPage";
+import SchoolSearchPage from "./pages/SchoolSearchPage";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/search" exact={true} component={SchoolMealsPage}></Route>
+      <Route path="/" component={SchoolSearchPage}></Route>
+    </Router>
   );
 }
 
