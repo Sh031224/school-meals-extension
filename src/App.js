15d922ec234e253e2c47e9cf787fd42e2d65ceb1
Sh031224 <1cktmdgh2@gmail.com>
2020-05-18 20:43:33 +0900

    [ADD] Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Router, goTo } from "react-chrome-extension-router";
 import SchoolMealsPage from "./pages/SchoolMealsPage";
 import SchoolSearchPage from "./pages/SchoolSearchPage";
 import "./App.scss";
 
 function App() {
+  useEffect(() => {
+    const keys = ["school_id", "office_code"];
+    /* global chrome */
+    chrome.storage.sync.get(keys, (items) => {
+      if (!items.office_code || !items.school_id) {
+        goTo(SchoolSearchPage);
+      }
+    });
+  }, []);
+
   return (
     <Router>
-      <Route path="/" exact={true} component={SchoolMealsPage}></Route>
-      <Route path="/search" component={SchoolSearchPage}></Route>
+      <SchoolMealsPage />
     </Router>
   );
 }
