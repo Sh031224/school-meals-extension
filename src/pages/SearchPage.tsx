b35c59ed27295153e41d4335f30c124071e1f22b
Sh031224 <1cktmdgh2@gmail.com>
2020-09-26 19:16:09 +0900

    [ADD] lib

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchContainer from "../containers/Search/SearchContainer";
+
+const SearchPage = () => {
+  return (
+    <>
+      <SearchContainer />
+    </>
+  );
+};
+
+export default SearchPage;
