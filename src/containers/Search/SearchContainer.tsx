03944bc84fb8f374334ebb7123266ee1026c671b
Sh031224 <1cktmdgh2@gmail.com>
2020-09-27 15:19:03 +0900

    [ADD] SearchContainer

--- src/containers/Search/SearchContainer.tsx
+++ src/containers/Search/SearchContainer.tsx
@@ -1,6 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import School from "../../assets/api/School";
+import getStorage from "../../lib/getStorage";
+import ResponseType from "../../types/Response";
+
+interface SchoolSearchResponse extends ResponseType {
+  data: {
+    school: Array<SchoolType>;
+  };
+}
+
+interface SchoolType {
+  school_name: string;
+  school_locate: string;
+  office_code: string;
+  school_id: number;
+}
 
 const SearchContainer = ({}) => {
+  const [goBack, setGoBack] = useState<boolean>(false);
+  const [schoolList, setSchoolList] = useState<Array<SchoolType>>([]);
+
+  const [search, setSearch] = useState<string>("");
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
+
+  const storageCheck = useCallback(() => {
+    const isExisit = getStorage();
+    if (isExisit.office_code && isExisit.school_id) {
+      setGoBack(true);
+    } else {
+      setGoBack(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    storageCheck();
+  }, [storageCheck]);
+
+  const getScheduleCallback = useCallback(async () => {
+    setLoading(true);
+    setSchoolList([]);
+    setIsEmpty(false);
+    await School.SearchSchool(search)
+      .then((res: SchoolSearchResponse) => {
+        setSchoolList(res.data.school);
+      })
+      .catch((err: Error) => {
+        if (err.message.indexOf("404") !== -1) {
+          setIsEmpty(true);
+        }
+      });
+    setLoading(false);
+  }, [search]);
+
   return (
     <>
       <div></div>
