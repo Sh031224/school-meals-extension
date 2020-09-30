d31b6c158872d018db6689f98465bcf5282ee18b
Sh031224 <1cktmdgh2@gmail.com>
2020-09-30 18:19:03 +0900

    [ADD] searchList

---
+++ src/components/Search/SearchList/SearchListItem/SearchListItem.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./SearchListItem.scss";
+
+interface SearchListItemProps {
+  item: SchoolType;
+  idx: number;
+  selectSchool: (idx: number) => void;
+}
+
+interface SchoolType {
+  school_name: string;
+  school_locate: string;
+  office_code: string;
+  school_id: number;
+}
+
+const SearchListItem = ({ item, idx, selectSchool }: SearchListItemProps) => {
+  return (
+    <div className="search_list_item" onClick={() => selectSchool(idx)}>
+      <div className="search_list_item_name" title={item.school_locate}>
+        {item.school_name}
+      </div>
+    </div>
+  );
+};
+
+export default SearchListItem;
